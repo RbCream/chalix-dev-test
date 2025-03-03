004f33fcd620576cd1e526fee0d78e7a93c0c6cd
Rbcream <khj0633@gmail.com>
2025-03-03 14:44:15 +0900

    feat: Create project
    create front&back project
    default setting

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import MainPage from './pages/MainPage';
+import BoardPage from './pages/BoardPage';
+import './styles/App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/board/presentation" element={<BoardPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
